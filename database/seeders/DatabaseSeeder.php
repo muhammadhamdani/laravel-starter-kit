39095bb4d3d312b92cfffcefe685c57c64367707
Yatim Mandiri <muhammadhamdani017@gmail.com>
2025-07-11 09:32:31 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -45,8 +45,8 @@ class DatabaseSeeder extends Seeder
         User::factory(1000)->create()->each(fn($user) => $user->assignRole('Users'));
 
         User::create([
-            'name' => 'Admin',
-            'email' => 'muhammadhamdani017@gmail.com',
+            'name' => 'Administrator',
+            'email' => 'admin@example.com',
             'password' => Hash::make('password'),
         ])->assignRole('Administrators');
     }
