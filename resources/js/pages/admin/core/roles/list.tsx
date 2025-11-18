0fbcb17eb0afe4672762164c47d8a758b0cef577
Yatim Mandiri <muhammadhamdani017@gmail.com>
2025-11-18 18:23:23 +0700

    Update list.tsx

--- resources/js/pages/admin/core/roles/list.tsx
+++ resources/js/pages/admin/core/roles/list.tsx
@@ -27,7 +27,7 @@ export default function ListPage() {
 
     return (
         <AppLayout>
-            <Head title="Permissions List" />
+            <Head title="Roles List" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
                 <div className="border-sidebar-border/70 dark:border-sidebar-border relative flex min-h-screen flex-1 flex-col space-y-4 overflow-hidden rounded-xl border md:min-h-min">
                     <DataTableProvider
