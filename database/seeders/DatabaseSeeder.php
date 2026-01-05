e9eea24e000ade8edbced911ca0a146a393ce8bb
Yatim Mandiri <muhammadhamdani017@gmail.com>
2026-01-05 09:09:50 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,13 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Core\Role;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Core\User;
-use App\Models\Core\Permission;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
-use Spatie\Permission\PermissionRegistrar;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,52 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        app()[PermissionRegistrar::class]->forgetCachedPermissions();
-
-        collect([
-            ['name' => 'Administrators'],
-            ['name' => 'Users'],
-        ])->each(fn($role) => Role::create($role));
-
-        collect([
-            ['name' => 'view-permission'],
-            ['name' => 'create-permission'],
-            ['name' => 'update-permission'],
-            ['name' => 'delete-permission'],
-            ['name' => 'data-permission'],
-            ['name' => 'view-role'],
-            ['name' => 'create-role'],
-            ['name' => 'update-role'],
-            ['name' => 'delete-role'],
-            ['name' => 'data-role'],
-            ['name' => 'view-user'],
-            ['name' => 'create-user'],
-            ['name' => 'update-user'],
-            ['name' => 'delete-user'],
-            ['name' => 'verify-user'],
-            ['name' => 'bulk-user'],
-            ['name' => 'data-user'],
-            ['name' => 'view-region'],
-            ['name' => 'create-region'],
-            ['name' => 'update-region'],
-            ['name' => 'delete-region'],
-            ['name' => 'data-region'],
-            ['name' => 'view-settings-site'],
-        ])->each(fn($permission) => Permission::create($permission)->assignRole('Administrators'));
-
-        User::create([
-            'name' => 'Administrator',
-            'email' => 'muhammadhamdani017@gmail.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make(uniqid()),
-        ])->assignRole('Administrators');
-
-        User::factory(100)->create()->each(function ($user) {
-            $user->assignRole('Users');
-        });
-
         $this->call([
+            UserRolePermissionSeeder::class,
             IndoRegionSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
